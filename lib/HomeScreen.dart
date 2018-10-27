6e8476753195bd3775153204f4d823193d54475d
eduardolujan <infinito15A>
2018-10-27 15:39:41 -0500

    Simplified Buttons List

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -8,8 +8,32 @@ import 'package:flutter/material.dart';
   class _HomeScreenState extends State<HomeScreen> {
     Color _myColor = Colors.black;
 
+
+
     @override
     Widget build(BuildContext context) {
+      final botones = <Widget>[
+       RaisedButton(child: Text("Azul",  style:TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blue
+      ), ), onPressed: ()=> _changeColor(Colors.blue),  ),
+       RaisedButton(child: Text("Verde", style:TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.green
+      ),), onPressed:  ()=> _changeColor(Colors.green)
+      ),
+       RaisedButton(child: Text("Rojo", style:TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.red
+      )
+      )
+       )
+      ];
+
+
       return Scaffold(
         appBar: AppBar(
           title: Text('My App'),
@@ -25,27 +49,7 @@ import 'package:flutter/material.dart';
             ),
             new Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new RaisedButton(child: Text("Azul",  style:TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
-                ), ), onPressed: ()=> _changeColor(Colors.blue),  ),
-                new RaisedButton(child: Text("Verde", style:TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green
-                ),), onPressed:  ()=> _changeColor(Colors.green)
-                ),
-                new RaisedButton(child: Text("Rojo", style:TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red
-                ),), onPressed:  ()=> _changeColor(Colors.red)),
-
-              ],
-
-
+              children: botones
             )
 
           ],
