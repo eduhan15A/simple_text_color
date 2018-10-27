e9a97f1438de607736016d10e6e3e174b2c54a5f
eduardolujan <infinito15A>
2018-10-27 15:19:22 -0500

    Initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+  class HomeScreen extends StatefulWidget {
+    @override
+    _HomeScreenState createState() => _HomeScreenState();
+  }
+
+  class _HomeScreenState extends State<HomeScreen> {
+    Color _myColor = Colors.black;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[new Text("Cambia el color del Texto",
+                style:TextStyle(
+               fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                color: _myColor
+            ),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new RaisedButton(child: Text("Azul",  style:TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue
+                ), ), onPressed: ()=> _changeColor(Colors.blue),  ),
+                new RaisedButton(child: Text("Verde", style:TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green
+                ),), onPressed:  ()=> _changeColor(Colors.green)
+                ),
+                new RaisedButton(child: Text("Rojo", style:TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red
+                ),), onPressed:  ()=> _changeColor(Colors.red)),
+
+              ],
+
+
+            )
+
+          ],
+
+
+        )
+
+
+      );
+
+    }
+    void _changeColor(Color color){
+
+      setState(() {
+        _myColor = color;
+      });
+
+    }
+  }
